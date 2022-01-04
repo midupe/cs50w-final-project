bd9001cf5e46b3b6f05bc05d09a6a95cd35bd931
midupe <miguelpereira2000@gmail.com>
2022-01-04 19:10:42 +0000

    up

--- cs50wfinal/models.py
+++ cs50wfinal/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Url(models.Model):
+    url = models.CharField(max_length=254, null=False, blank=False)
+    shorten = models.CharField(max_length=254, unique=True, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    visits = models.IntegerField(default=0)
+
+    
--- cs50wfinal/views.py
+++ cs50wfinal/views.py
@@ -1,11 +1,38 @@
-from django.http.response import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.http.response import Http404, HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import render
+import random, string
 
+from .models import *
+
+def create_url(request, url):
+    user = request.user if request.user.is_authenticated else None
+    letters = string.ascii_lowercase
+
+    try:
+        u = Url(url=url, shorten=''.join(random.choice(letters) for i in range(10)), user=user)
+        u.save()
+    except:
+        #retry if shorten generate gives already exists
+        u = Url(url=url, shorten=''.join(random.choice(letters) for i in range(10)), user=user)
+        u.save()
 
 def index(request):
     results = {}
-    return render(request, "prod.html", results)
+
+    if request.method == "POST":
+        create_url(request, request.POST["url"])
+    return render(request, "index.html", results)
 
 def login(request):
     results = {}
     return render(request, "login.html", results)
+
+def shorten(request,shorten):
+    try:
+        u = Url.objects.get(shorten=shorten)
+        url = u.url
+        u.visits +=1
+        u.save()
+        return HttpResponseRedirect(url)
+    except:
+        return HttpResponse("<h1>Nothing Found</h1><h2>404</h2>")
